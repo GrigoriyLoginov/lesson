9c93ff31e1aaeb3a97c01819e9f542a92d791a04
GrigoriyLoginov <50609381+GrigoriyLoginov@users.noreply.github.com>
2019-06-04 17:38:41 +0400

    Add files via upload

---
+++ lessons/Task4_6/Filter.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Task4_6
+{
+    public static class Filter
+    {
+        
+        public static int[] FilterDigit(int[] array, int digit)
+        {
+            if (array is null || digit < 0 || digit > 9)
+            {
+                throw new ArgumentNullException();
+            }
+
+            List<int> buff = new List<int>();
+            for (int i = 0; i < array.Length; i++)
+            {
+
+                int temp = array[i];
+                while (temp > 0)
+                {
+                    if (temp % 10 == digit)
+                        buff.Add(array[i]);
+                    temp /= 10;
+                }
+            }
+            if (buff.ToArray<int>().Length == 0) return null;
+            return buff.ToArray<int>();
+        }
+
+    }
+}
